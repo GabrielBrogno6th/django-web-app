a5970a2ed8fcf076b6a3145312e2050187d9d873
GabrielBrogno6th <gabriel.brogno@outlook.com>
2026-04-26 22:32:49 -0300

    Word counter added.

--- app/models.py
+++ app/models.py
@@ -22,6 +22,14 @@ class UserProfile(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
+    @property
+    def article_count(self):
+        return self.articles.count()
+    
+    @property
+    def written_words(self):
+        return self.articles.aggregate(models.Sum("word_count"))["word_count__sum"] or 0
+
 class Article(models.Model):
     title = models.CharField(_("title"), max_length=100)
     content = models.TextField(_("content"),blank=True, default="")
